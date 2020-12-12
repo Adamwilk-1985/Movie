d4708259a92e2aff376ce16c7f9e903e356ae4b9
User <wujekwlq@gmail.com>
2020-12-12 10:22:42 +0100

    Movie budget

---
+++ MovieDB/src/com/Director.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Director extends Staff {
+
+    public Director(String firstName, String lastName, double payment) {
+        super(firstName, lastName, payment * 3);
+    }
+}
